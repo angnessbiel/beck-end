b72b58bf5a78c0a0679b540bd41e2094b30e1a56
GABRIEL MENDES ANGNES <gabriel_angnes@estudante.sesisenai.org.br>
2024-07-04 16:28:49 -0300

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-function filtrarPares(arr){
-    nova_arr = []
-    for(let i=0; i < arr.length; i++){
-        if (arr[i] % 2 === 0){
-            nova_arr.push(arr[i])
-        }
-    }
-    return nova_arr
-}
+const express = require('express')
 
-arr = [1, 2, 3, 4, 5]
-resultado = filtrarPares(arr)
-console.log(resultado)
+const app = express()
+
+app.get('/test-api', function (req, res){
+    res.send('NOSSA API TA FUNCIONANDO')
+})
+
+app.listen(8000)
